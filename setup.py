d2a44092de1a23c5faa1adae682b561a817b114c
Ian Dennis Miller <ian@iandennismiller.com>
2017-12-11 11:19:05 -0500

    python 2 only

--- setup.py
+++ setup.py
@@ -30,13 +30,24 @@ setup(
         "bin/regdel",
     ],
     long_description=read('Readme.rst'),
-    classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Office/Business :: Financial :: Accounting',
+    ],
     include_package_data=True,
     keywords='',
     author=grep('__author__'),
     author_email=grep('__email__'),
     url=grep('__url__'),
     install_requires=read('requirements.txt'),
+    dependency_links=[
+        'https://github.com/Rudd-O/ledgerhelpers/archive/v0.2.3.tar.gz#egg=ledgerhelpers',
+    ],
     license='MIT',
     zip_safe=False,
 )
